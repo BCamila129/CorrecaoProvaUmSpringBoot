04ad0198f5bf097c0010c9c5990a0238f19d003d
BCamila129 <camilabruna129@gmail.com>
2022-11-18 18:58:21 -0300

    correcao prova

---
+++ src/main/java/com/example/CorrecaoProvaUmSpringBoot/CorrecaoProvaUmSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.CorrecaoProvaUmSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorrecaoProvaUmSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorrecaoProvaUmSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CorrecaoProvaUmSpringBoot/CorrecaoProvaUmSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CorrecaoProvaUmSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorrecaoProvaUmSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
